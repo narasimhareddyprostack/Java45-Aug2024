ea6ae0df42519328253faf219783d062c2f7601c
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-08-27 13:19:46 +0530

    Ts

---
+++ Class-1-Basics/Test.java
@@ -0,0 +1,13 @@
+class Test{
+	public static void main(String[] args){
+		int a = 10;
+		int b=20;
+		System.out.println(a+b);
+	}
+}
+class A{}
+class B{}
+class C{}
+class D{}
+class E{}
+class F{}
---
+++ class-2 -Basics/Test.java
@@ -0,0 +1,8 @@
+class Test{
+	
+	public static void main(String[] args){
+		int a=10;
+		a="Rahul";
+		System.out.println(a);
+	}
+}
