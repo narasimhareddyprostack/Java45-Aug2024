1d31b6be9741572b201b86d1dffb28af4c2ce34c
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-12-19 14:10:56 +0530

    Tst

---
+++ Backend/SB-All/db-demo2/src/main/java/com/pro/jt2/DbDemo2Application.java
@@ -0,0 +1,17 @@
+package com.pro.jt2;
+
+import com.pro.jt2.jdbc.DBImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbDemo2Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbDemo2Application.class, args);
+
+    }
+
+}
