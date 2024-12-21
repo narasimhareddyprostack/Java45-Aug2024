c6315757af6d516014b16dc3807b50b68fb75e8b
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-12-21 13:28:49 +0530

    t

---
+++ Backend/SB-All/webapis/webapis/src/main/java/com/pro/webapis/WebapisApplication.java
@@ -0,0 +1,13 @@
+package com.pro.webapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapisApplication.class, args);
+	}
+
+}
